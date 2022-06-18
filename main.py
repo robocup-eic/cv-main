86497ce4e681a471b2e4d52581c874552ce38437
Pon-ek Tangmunchittham <pon-ek@hotmail.com>
2022-06-18 16:10:48 +0700

    Add state for process handling (Not finished)

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import subprocess
 import threading
+import signal
 import os
+import time
+from argparse import ArgumentParser
+from py import process
 import yaml
 
 
@@ -16,30 +20,54 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-def read_config():
-    with open("config.yaml") as file:
+running_processes = dict()
+
+
+def read_config(config_path):
+    with open(config_path) as file:
         configs = yaml.load(file, Loader=yaml.FullLoader)
+
+    if "python-exec" not in configs or configs["python-exec"] is None:
+        configs["python-exec"] = "python"
+        print("python-exec is not set in config.yaml, uses 'python' as default value")
+    if "processes" not in configs:
+        raise ValueError("There is no processes provided in config.yaml")
+    if "states" not in configs or configs["states"] is None:
+        configs["states"] = [{"default": [k for k in configs["processes"]]}]
+        print("states is not set in config.yaml, created state 'default' with all processes started")
+
+    python_exec = configs["python-exec"]
     
-    output = list()
-    for name, conf in configs.items():
+    processes = list()
+    process_names = set()
+    for name, conf in configs["processes"].items():
         if "exec_path" not in conf or "conda_env" not in conf:
-            raise ValueError("Model '%s' in config.yaml must contain both 'exec_path' and 'conda_env' fields" % name)
+            raise ValueError("Process '%s' in config.yaml must contain both 'exec_path' and 'conda_env' fields" % name)
         if not isinstance(conf["exec_path"], str):
-            raise ValueError("Value of 'exec_path' in model '%s' must be string" % name)
+            raise ValueError("Value of 'exec_path' in process '%s' must be string" % name)
         if not isinstance(conf["conda_env"], str):
-            raise ValueError("Value of 'conda_env' in model '%s' must be string" % name)
+            raise ValueError("Value of 'conda_env' in process '%s' must be string" % name)
         env_name = conf["conda_env"]
         script_path = os.path.normpath(conf["exec_path"])
-        output.append((name, env_name, script_path))
-    return output
+        process_names.add(name)
+        processes.append((name, env_name, script_path))
+    
+    states = configs["states"]
+    for state, procs in configs["states"].items():
+        if not set(procs).issubset(process_names):
+            raise ValueError("Some processes in state '%s' are not included in processes" % state)
+
+    return python_exec, processes, states
 
 
-def run_new_process(env_name, script_path, process_name, color):
+def run_new_process(env_name, python_exec, script_path, process_name, color):
     print(color + "Starting %s..." % process_name + bcolors.ENDC)
     print_tag = color + "%15s | " % process_name + bcolors.ENDC
-    cmds = ["conda", "activate", env_name, "&&", "python", script_path, "&&", "conda", "deactivate"]
+    cmds = ["conda", "activate", env_name, "&&", python_exec, script_path, "&&", "conda", "deactivate"]
 
     p = subprocess.Popen(cmds, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, encoding='utf-8', shell=True)
+    running_processes[process_name] = p
+
     while True:
         output = p.stdout.readline()
         if output == '' and p.poll() is not None:
@@ -48,15 +76,37 @@ def run_new_process(env_name, script_path, process_name, color):
             print(print_tag + output.rstrip("\n"), flush=True)
     print(color + "%s has stopped" % process_name + bcolors.ENDC)
 
+    running_processes.pop(process_name, None)
 
-def main():
+
+def main(args):
+    p_count = 0
     colors = [bcolors.OKPURPLE, bcolors.OKBLUE, bcolors.OKCYAN, bcolors.OKGREEN, bcolors.OKYELLOW]
-    configs = read_config()
-    for i, (process_name, env_name, script_path) in enumerate(configs):
-        s = threading.Thread(target=run_new_process, args=(env_name, script_path, process_name, colors[i % len(colors)]))
-        s.start()
+    python_exec, processes, states = read_config(args["configpath"])
+    for (process_name, env_name, script_path) in processes:
+        if process_name not in running_processes or running_processes[process] is None:
+            s = threading.Thread(target=run_new_process, args=(env_name, python_exec, script_path, process_name, colors[p_count % len(colors)]))
+            s.start()
+            p_count += 1
+    
+    ### Start Testing ###
+    time.sleep(10)
+    print(running_processes)
+    running_processes["process1"].send_signal(signal.CTRL_C_EVENT)
+    time.sleep(1)
+    print(running_processes)
+    s = threading.Thread(target=run_new_process, args=("testenv", "python", "test.py", "process1", colors[p_count % len(colors)]))
+    s.start()
+    p_count += 1
+    time.sleep(5)
+    print(running_processes)
+    ### End Testing ###
 
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("-f", "--fileconfig", dest="configpath", default="config.yaml",
+                        help="path to configurations .yaml file", metavar="FILEPATH")
+    args = vars(parser.parse_args())
     os.environ["PYTHONUNBUFFERED"] = "1"
-    main()
+    main(args)
---
+++ test.py
@@ -0,0 +1,9 @@
+import time
+
+print("start test.py")
+count = 0
+
+while True:
+    print(count)
+    time.sleep(1)
+    count += 1
